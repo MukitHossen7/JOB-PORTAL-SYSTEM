3bfc7c924d489e2fd027a6cecfa876f7917f076c
MukitHossen7 <hossenmukit7@gmail.com>
2024-12-10 22:08:39 +0600

    resgister page design is complete

--- src/Pages/Authentication/Register/Register.jsx
+++ src/Pages/Authentication/Register/Register.jsx
@@ -1,41 +1,67 @@
+import Lottie from "lottie-react";
+import animationRegister from "../../../assets/Lottie/register.json";
+import { useForm } from "react-hook-form";
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const handleRegister = (data) => {
+    console.log(data);
+    const name = data.name;
+    const photo = data.photo;
+    const email = data.email;
+    const password = data.password;
+    console.log(name, photo, email, password);
+  };
   return (
     <div>
       <div className="hero  min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left w-full lg:w-1/2">
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
+            <Lottie animationData={animationRegister}></Lottie>
           </div>
           <div className="card bg-base-100 w-full max-w-xl shrink-0 shadow-md lg:w-1/2">
             <h1 className="text-4xl font-bold text-center py-4">
               Register now!
             </h1>
-            <form className="card-body">
+            <form className="card-body" onSubmit={handleSubmit(handleRegister)}>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Name*</span>
                 </label>
                 <input
-                  type="text"
+                  {...register("name", {
+                    required: {
+                      value: true,
+                      message: "Please enter your name",
+                    },
+                  })}
                   placeholder="full name"
                   className="input input-bordered"
-                  required
                 />
+                <span className="text-xs text-red-500 font-semibold mt-1">
+                  {errors.name?.message}
+                </span>
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Photo URL*</span>
                 </label>
                 <input
-                  type="text"
+                  {...register("photo", {
+                    required: {
+                      value: true,
+                      message: "Please enter a photo URL",
+                    },
+                  })}
                   placeholder="photo URL"
                   className="input input-bordered"
-                  required
                 />
+                <span className="text-xs text-red-500 font-semibold mt-1">
+                  {errors.photo?.message}
+                </span>
               </div>
 
               <div className="form-control">
@@ -43,22 +69,47 @@ const Register = () => {
                   <span className="label-text">Email*</span>
                 </label>
                 <input
-                  type="email"
+                  {...register("email", {
+                    required: {
+                      value: true,
+                      message: "Please enter a valid email address",
+                    },
+                  })}
                   placeholder="email"
                   className="input input-bordered"
-                  required
                 />
+                {errors.email && (
+                  <span className="text-xs text-red-500 font-semibold mt-1">
+                    {errors.email?.message}
+                  </span>
+                )}
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password*</span>
                 </label>
                 <input
-                  type="password"
+                  {...register("password", {
+                    required: {
+                      value: true,
+                      message: "Password is required",
+                    },
+                    minLength: {
+                      value: 6,
+                      message: "Password must be at least 6 characters",
+                    },
+                    pattern: {
+                      value: /^(?=.*[a-z])(?=.*[A-Z]).{2,}$/,
+                      message:
+                        "Password must contain at least one uppercase letter, one lowercase letter",
+                    },
+                  })}
                   placeholder="password"
                   className="input input-bordered"
-                  required
                 />
+                <span className="text-xs text-red-500 font-semibold mt-1">
+                  {errors.password?.message}
+                </span>
               </div>
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Register</button>
