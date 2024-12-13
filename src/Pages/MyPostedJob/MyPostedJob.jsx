0bf063d3ef96965407f74d6baa3f432c51b2db26
MukitHossen7 <hossenmukit7@gmail.com>
2024-12-13 20:14:33 +0600

    my posted job  design is complete

--- src/Pages/MyPostedJob/MyPostedJob.jsx
+++ src/Pages/MyPostedJob/MyPostedJob.jsx
@@ -10,10 +10,56 @@ const MyPostedJob = () => {
       .then((res) => res.json())
       .then((data) => setJobs(data));
   }, [user.email]);
-  console.log(jobs);
   return (
-    <div>
-      <h2>My Posted jobs</h2>
+    <div className="pt-12 pb-20">
+      <h3 className=" text-3xl text-center pb-8 font-semibold">
+        My Posted Job
+      </h3>
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job Title</th>
+              <th>HR Name</th>
+              <th>HR Email</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {jobs.map((job, idx) => (
+              <tr key={idx}>
+                <th> {idx + 1} </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={job?.company_logo}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{job?.company}</div>
+                      <div className="text-sm opacity-50">{job?.location}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>{job?.title}</td>
+                <td>{job?.hr_name}</td>
+                <td>{job?.hr_email}</td>
+                <th>
+                  <button className="btn bg-red-500 hover:bg-red-500 text-white btn-xs">
+                    X
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
